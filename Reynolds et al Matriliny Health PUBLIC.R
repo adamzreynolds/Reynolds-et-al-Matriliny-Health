1b18473ab07e3fb827a64f4edc4cffcf6ad6b3b4
adamzreynolds <51865169+adamzreynolds@users.noreply.github.com>
2020-08-24 16:49:15 -0600

    Analyses for Reynolds et al.

--- Reynolds et al Matriliny Health PUBLIC.R
+++ Reynolds et al Matriliny Health PUBLIC.R
@@ -7,7 +7,7 @@
 #-------------------------------------------------
 # READ DATA FROM FILE
 
-data <- read.csv("Data_S1.csv")
+data <- read.csv("Dataset_S1.csv")
 
 
 
@@ -16,49 +16,45 @@ data <- read.csv("Data_S1.csv")
 
 require(rethinking)
 
-# MAKE DUMMIES
-data$Pat <- ifelse(data$MP=="Patrilineal", 1, 0)
-data$Mat <- ifelse(data$MP=="Matrilineal", 1, 0)
-
-# CENTER AGE
-data$Age.c <- data$Age - mean(data$Age)
-
-# SUBSET ONLY COMPLETE DATA
-d2 <- data[!is.na(data$BinaryCat),]
 
 
 #------------------------------------
+# SUBSET ONLY COMPLETE DATA
+d2 <- data[!is.na(data$elevatedCRP),]
+
 # CRP MAIN MODEL, REF: PAT FEMALES
 start <- list(
-	a = mean(d2$BinaryCat),
+	a = mean(d2$elevatedCRP),
 	b.Age = 0,
 	b.Male = 0,
-	b.Mat = 0,
-	b.Male.Mat = 0
+	b.Matriliny = 0,
+	b.Male.Matriliny = 0
 )
 flist <- alist(
-	BinaryCat ~ dbinom(1,p),
-	logit(p) <- a + b.Male*Male + b.Mat*Mat + b.Male.Mat*Male*Mat + b.Age*Age,
+	elevatedCRP ~ dbinom(1,p),
+	logit(p) <- a + b.Male*Male + b.Matriliny*Matriliny + b.Male.Matriliny*Male*Matriliny + b.Age*Age,
 	a ~ dnorm(-10,10),
 	b.Age ~ dnorm(0,10),
 	b.Male ~ dnorm(0,10),
-	b.Mat ~ dnorm(0,10),
-	b.Male.Mat ~ dnorm(0,10)
+	b.Matriliny ~ dnorm(0,10),
+	b.Male.Matriliny ~ dnorm(0,10)
 )
 mCRP.patF <- map(flist, data=d2, start=start)
 precis(mCRP.patF, prob=0.95)
 post.patF <- extract.samples(mCRP.patF, 1e4)
 precis(post.patF, prob=.95)
-# dens(post.patF)
+dens(post.patF)
 
 # what is the posterior prob that the age param is < 0?
 sum(post.patF$b.Age < 0) / 1e4
 # what is the posterior probability that the Mat parameter is > zero?
-sum(post.patF$b.Mat > 0) / 1e4 
+sum(post.patF$b.Matriliny > 0) / 1e4 
 # what is the posterior prob that male param is > 0
-sum(post.patF$b.Male < 0) / 1e4
-# what is the posterior prob that the male-Mat param is < 0?
-sum(post.patF$b.Male.Mat > 0) / 1e4
+sum(post.patF$b.Male > 0) / 1e4
+# what is the posterior prob that the male-Matriliny param is < 0?
+sum(post.patF$b.Male.Matriliny < 0) / 1e4
+
+
 
 
 #---------------------------------
@@ -69,20 +65,20 @@ d3 <- d2
 d3 <- d3[!is.na(d3$Head),]
 
 start <- list(
-	a = mean(d2$BinaryCat),
+	a = mean(d2$elevatedCRP),
 	b.Age = 0,
 	b.Male = 0,
-	b.Mat = 0,
-	b.Male.Mat = 0,
+	b.Matriliny = 0,
+	b.Male.Matriliny = 0,
 	b.Head = 0)
 flist <- alist(
-	BinaryCat ~ dbinom(1,p),
-	logit(p) <- a + b.Age*Age + b.Head*Head + b.Male*Male + b.Mat*Mat + b.Male.Mat*Male*Mat,
+	elevatedCRP ~ dbinom(1,p),
+	logit(p) <- a + b.Age*Age + b.Head*Head + b.Male*Male + b.Matriliny*Matriliny + b.Male.Matriliny*Male*Matriliny,
 	a ~ dnorm(-10,10),
 	b.Age ~ dnorm(0,10),
 	b.Male ~ dnorm(0,10),
-	b.Mat ~ dnorm(0,10),
-	b.Male.Mat ~ dnorm(0,10),	
+	b.Matriliny ~ dnorm(0,10),
+	b.Male.Matriliny ~ dnorm(0,10),	
 	b.Head ~ dnorm(0,10))
 mCRP.hoh <- map(flist, data=d3, start=start)
 precis(mCRP.hoh, prob=0.90)
@@ -92,8 +88,8 @@ precis(post.hoh, prob=.95)
 
 sum(post.hoh$b.Age < 0) / 1e4
 sum(post.hoh$b.Male > 0) / 1e4
-sum(post.hoh$b.Mat < 0) / 1e4
-sum(post.hoh$b.Male.Mat < 0) / 1e4
+sum(post.hoh$b.Matriliny > 0) / 1e4
+sum(post.hoh$b.Male.Matriliny < 0) / 1e4
 sum(post.hoh$b.Head > 0) / 1e4
 
 
@@ -104,32 +100,32 @@ sum(post.hoh$b.Head > 0) / 1e4
 
 #----------------------------------
 # HTN MAIN MODEL, REF: PAT FEMALES
-CYhtn <- CY[!is.na(CY$htn) & !is.na(CY$Male) & !is.na(CY$Matriliny) & !is.na(CY$Age),]
+dataHTN <- data[!is.na(data$htn) & !is.na(data$Male) & !is.na(data$Matriliny) & !is.na(data$Age),]
 
-with(CYhtn, table(htn, Male, Matriliny))
+with(dataHTN, table(htn, Male, Matriliny))
 
 start <- list(
-	a = mean(CYhtn$htn),	
+	a = mean(dataHTN$htn),	
 	b.Age = 0,
 	b.Male = 0,
-	b.Mat = 0,
-	b.Male.Mat = 0
+	b.Matriliny = 0,
+	b.Male.Matriliny = 0
 )
 flist <- alist(
 	htn ~ dbinom(1,p),
-	logit(p) <- a + b.Age*Age + b.Male*Male + b.Mat*Matriliny + b.Male.Mat*Male*Matriliny,
+	logit(p) <- a + b.Age*Age + b.Male*Male + b.Matriliny*Matriliny + b.Male.Matriliny*Male*Matriliny,
 	a ~ dnorm(-10,10),
 	b.Age ~ dnorm(0,10),
 	b.Male ~ dnorm(0,10),
-	b.Mat ~ dnorm(0,10),
-	b.Male.Mat ~ dnorm(0,10)
+	b.Matriliny ~ dnorm(0,10),
+	b.Male.Matriliny ~ dnorm(0,10)
 )
-mHTN <- map(flist, data=CYhtn, start=start)
+mHTN <- map(flist, data=dataHTN, start=start)
 
 precis(mHTN, prob=0.95)
 post.htn <- extract.samples(mHTN, 1e4)
 precis(post.htn, prob=.95)
-# dens(post.htn)
+dens(post.htn)
 # HPDI(post.htn$b.Pat, prob=.90)  # At prob=0.90 is the highest we can go while excluding the possibility of zero for the pat term
 
 # what is the posterior prob that the Age param is > 0?
@@ -140,87 +136,72 @@ sum(post.htn$b.Age < 0) / 1e4
 sum(post.htn$b.Male > 0) / 1e4
 sum(post.htn$b.Male < 0) / 1e4	
 
-# what is the posterior probability that the mat parameter is > zero?
-sum(post.htn$b.Mat >  0) / 1e4 
-sum(post.htn$b.Mat < 0) / 1e4 
+# what is the posterior probability that the Matriliny parameter is > zero?
+sum(post.htn$b.Matriliny >  0) / 1e4 
+sum(post.htn$b.Matriliny < 0) / 1e4 
 
 # what is the posterior prob that the male-pat param is < 0?
-sum(post.htn$b.Male.Mat < 0) / 1e4
-sum(post.htn$b.Male.Mat > 0) / 1e4
+sum(post.htn$b.Male.Matriliny < 0) / 1e4
+sum(post.htn$b.Male.Matriliny > 0) / 1e4
 
 
 #---------------------------------------
 # HTN HOH MODEL, REF: PAT FEMALES
 
-CYhtn$Head <- NA
-CYhtn$Head[CYhtn$RelToHeadOfHH!="Head of Household"] <- 0
-CYhtn$Head[CYhtn$RelToHeadOfHH=="Head of Household"] <- 1
-
-CYhtn3 <- CYhtn[!is.na(CYhtn$Head),]
+dataHTN3 <- dataHTN[!is.na(dataHTN$Head),]
 
 start <- list(
-	a = mean(CYhtn$htn),
+	a = mean(dataHTN$htn),
 	b.Age = 0,
 	b.Male = 0,
-	b.Mat = 0,
-	b.Male.Mat = 0,
+	b.Matriliny = 0,
+	b.Male.Matriliny = 0,
 	b.Head = 0)
 flist <- alist(
 	htn ~ dbinom(1,p),
-	logit(p) <- a + b.Age*Age + b.Head*Head + b.Male*Male + b.Mat*Matriliny + b.Male.Mat*Male*Matriliny,
+	logit(p) <- a + b.Age*Age + b.Head*Head + b.Male*Male + b.Matriliny*Matriliny + b.Male.Matriliny*Male*Matriliny,
 	a ~ dnorm(-10,10),
 	b.Age ~ dnorm(0,10),
 	b.Male ~ dnorm(0,10),
-	b.Mat ~ dnorm(0,10),
-	b.Male.Mat ~ dnorm(0,10),	
+	b.Matriliny ~ dnorm(0,10),
+	b.Male.Matriliny ~ dnorm(0,10),	
 	b.Head ~ dnorm(0,10))
-mHTN.head4 <- map(flist, data=CYhtn3, start=start)
+mHTN.head4 <- map(flist, data=dataHTN3, start=start)
 precis(mHTN.head4, prob=0.90)
 post4 <- extract.samples(mHTN.head4, 1e4)
 precis(post4, prob=.95)
 
 sum(post4$b.Age < 0) / 1e4
 sum(post4$b.Male > 0) / 1e4
-sum(post4$b.Mat > 0) / 1e4
-sum(post4$b.Male.Mat < 0) / 1e4
+sum(post4$b.Matriliny > 0) / 1e4
+sum(post4$b.Male.Matriliny < 0) / 1e4
 sum(post4$b.Head < 0) / 1e4
 
 
 
-
-#-----------------------------------------
-# CONTROLLING FOR BMI
-
-# Descriptives
-mean(CY$OverObese[CY$Male==0 & CY$Matriliny==0], na.rm=T)
-mean(CY$OverObese[CY$Male==1 & CY$Matriliny==0], na.rm=T)
-mean(CY$OverObese[CY$Male==0 & CY$Matriliny==1], na.rm=T)
-mean(CY$OverObese[CY$Male==1 & CY$Matriliny==1], na.rm=T)
-
-# boxplot(CY$BMI[CY$Male==0 & CY$Matriliny==0], CY$BMI[CY$Male==1 & CY$Matriliny==0], CY$BMI[CY$Male==0 & CY$Matriliny==1], CY$BMI[CY$Male==1 & CY$Matriliny==1], names=c("Pat Females", "Pat Males", "Mat Females", "Mat Males"), main="BMI")
-
+### TABLE S1
 #---------------------------------
 # CRP BMI MODEL, REF: PAT FEMALE
 
-d6 <- d2[!is.na(d2$BMI),]
+d6 <- d2[!is.na(d2$OverObese) & !is.na(d2$Underweight),]
 
 start <- list(
-	a = mean(d6$BinaryCat),
+	a = mean(d6$elevatedCRP),
 	b.Age = 0,
 	b.Male = 0,
-	b.Mat = 0,
-	b.Male.Mat = 0,
+	b.Matriliny = 0,
+	b.Male.Matriliny = 0,
 	b.OverObese = 0,
 	b.Underweight = 0
 )
 flist <- alist(
-	BinaryCat ~ dbinom(1,p),
-	logit(p) <- a + b.Male*Male + b.Mat*Mat + b.Male.Mat*Male*Mat + b.Age*Age + b.OverObese*OverObese + b.Underweight*Underweight,
+	elevatedCRP ~ dbinom(1,p),
+	logit(p) <- a + b.Male*Male + b.Matriliny*Matriliny + b.Male.Matriliny*Male*Matriliny + b.Age*Age + b.OverObese*OverObese + b.Underweight*Underweight,
 	a ~ dnorm(-10,10),
 	b.Age ~ dnorm(0,10),
 	b.Male ~ dnorm(0,10),
-	b.Mat ~ dnorm(0,10),
-	b.Male.Mat ~ dnorm(0,10),
+	b.Matriliny ~ dnorm(0,10),
+	b.Male.Matriliny ~ dnorm(0,10),
 	b.OverObese ~ dnorm(0,10),
 	b.Underweight ~ dnorm(0,10)
 )
@@ -234,16 +215,16 @@ sum(post.met$b.Age > 0) / 1e4
 sum(post.met$b.Age < 0) / 1e4
 
 # what is the posterior probability that the pat parameter is > zero?
-sum(post.met$b.Mat > 0) / 1e4 
-sum(post.met$b.Mat < 0) / 1e4 
+sum(post.met$b.Matriliny > 0) / 1e4 
+sum(post.met$b.Matriliny < 0) / 1e4 
 
 # what is the posterior prob that male param is > 0
 sum(post.met$b.Male > 0) / 1e4
 sum(post.met$b.Male < 0) / 1e4
 
 # what is the posterior prob that the male-pat param is < 0?
-sum(post.met$b.Male.Mat < 0) / 1e4
-sum(post.met$b.Male.Mat > 0) / 1e4
+sum(post.met$b.Male.Matriliny < 0) / 1e4
+sum(post.met$b.Male.Matriliny > 0) / 1e4
 
 # what is the posterior prob that the OverObese param is > 0?
 sum(post.met$b.OverObese > 0) / 1e4
@@ -258,29 +239,29 @@ sum(post.met$b.Underweight < 0) / 1e4
 #-------------------------------
 # HTN BMI MODEL, REF: PAT FEMALE
  
-CYhtn6 <- CY[!is.na(CY$htn) & !is.na(CY$Male) & !is.na(CY$Matriliny) & !is.na(CY$Age) & !is.na(CY$OverObese) & !is.na(CY$Underweight),]
+dataHTN6 <- dataHTN[!is.na(dataHTN$OverObese) & !is.na(dataHTN$Underweight),]
 
 start <- list(
-	a = mean(CYhtn6$htn),
+	a = mean(dataHTN6$htn),
 	b.Age = 0,
 	b.Male = 0,
-	b.Mat = 0,
-	b.Male.Mat = 0,
+	b.Matriliny = 0,
+	b.Male.Matriliny = 0,
 	b.OverObese = 0,
 	b.Underweight = 0
 )
 flist <- alist(
 	htn ~ dbinom(1,p),
-	logit(p) <- a + b.Male*Male + b.Mat*Matriliny + b.Male.Mat*Male*Matriliny + b.Age*Age + b.OverObese*OverObese + b.Underweight*Underweight,
+	logit(p) <- a + b.Male*Male + b.Matriliny*Matriliny + b.Male.Matriliny*Male*Matriliny + b.Age*Age + b.OverObese*OverObese + b.Underweight*Underweight,
 	a ~ dnorm(-10,10),
 	b.Age ~ dnorm(0,10),
 	b.Male ~ dnorm(0,10),
-	b.Mat ~ dnorm(0,10),
-	b.Male.Mat ~ dnorm(0,10),
+	b.Matriliny ~ dnorm(0,10),
+	b.Male.Matriliny ~ dnorm(0,10),
 	b.OverObese ~ dnorm(0,10),
 	b.Underweight ~ dnorm(0,10)
 )
-mHTN.bmi <- map(flist, data=CYhtn6, start=start)
+mHTN.bmi <- map(flist, data=dataHTN6, start=start)
 
 precis(mHTN.bmi, prob=0.95)
 post.htn.bmi <- extract.samples(mHTN.bmi, 1e4)
@@ -292,16 +273,16 @@ sum(post.htn.bmi$b.Age > 0) / 1e4
 sum(post.htn.bmi$b.Age < 0) / 1e4
 
 # what is the posterior probability that the pat parameter is > zero?
-sum(post.htn.bmi$b.Mat >  0) / 1e4 
-sum(post.htn.bmi$b.Mat < 0) / 1e4 
+sum(post.htn.bmi$b.Matriliny >  0) / 1e4 
+sum(post.htn.bmi$b.Matriliny < 0) / 1e4 
 
 # what is the posterior prob that male param is > 0
 sum(post.htn.bmi$b.Male > 0) / 1e4
 sum(post.htn.bmi$b.Male < 0) / 1e4
 
 # what is the posterior prob that the male-pat param is < 0?
-sum(post.htn.bmi$b.Male.Mat < 0) / 1e4
-sum(post.htn.bmi$b.Male.Mat > 0) / 1e4
+sum(post.htn.bmi$b.Male.Matriliny < 0) / 1e4
+sum(post.htn.bmi$b.Male.Matriliny > 0) / 1e4
 
 # what is the posterior prob that the OverObese param is > 0?
 sum(post.htn.bmi$b.OverObese > 0) / 1e4
@@ -310,6 +291,8 @@ sum(post.htn.bmi$b.OverObese < 0) / 1e4
 # what is the posterior prob that the Underweight param is > 0?
 sum(post.htn.bmi$b.Underweight > 0) / 1e4
 sum(post.htn.bmi$b.Underweight < 0) / 1e4
+
+
 
 
 
@@ -328,7 +311,7 @@ require(ggpubr)
 new_data <- data.frame(
 	Age = 45,
 	Male = c(0, 1, 0, 1),
-	Mat = c(0, 0, 1, 1)
+	Matriliny = c(0, 0, 1, 1)
 )
 
 p <- link(mCRP.patF, data=new_data)	
@@ -342,7 +325,7 @@ colnames(p2) <- c("mean", "lower", "upper")
 preds <- cbind(pdata, p2)
 
 preds$Gender <- c("Women", "Men", "Women", "Men")
-preds$Matriliny <- as.factor(preds$Mat)
+preds$Matriliny <- as.factor(preds$Matriliny)
 
 
 ### HTN, MAIN MODEL
@@ -409,5 +392,4 @@ ggarrange(CRP, HTN,
 		  labels = c("A", "B"),
 		  ncol = 2, nrow = 1,
 		  common.legend = TRUE, legend = "right")
-
 
